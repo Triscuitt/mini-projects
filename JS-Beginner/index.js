b107cfd826b61e9b4e7a3a25d1aec7076cb1b14f
Trjiscuit <trjistanbendoyr@gmail.com>
2026-04-10 23:06:18 +0800

    Created:     -JS-Beginner/index.html     -JS-Beginner/index.js     -JS-Beginner/style.css Updated:     -Javascript/javascript-algorithm.js -> JS-algorithm/javascript-algorithm.js Deleted:     -javascript-algorithm.js

---
+++ JS-Beginner/index.js
@@ -0,0 +1,20 @@
+// Use for Output
+console.log(`Hello`);
+console.log(`I like burgers`); 
+
+// Alert box
+// window.alert(`This is an alert!`);
+// window.alert(`I like burgers!`);
+
+// Change content of html element:
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById('myP').textContent = `I like burgers!`;
+
+
+// This is a comment
+/*
+    This 
+    is
+    a 
+    comment
+*/
