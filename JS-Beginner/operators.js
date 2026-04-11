ad09443a442a5a8d1caf76c2085ff3ce4e45b143
Trjiscuit <trjistanbendoyr@gmail.com>
2026-04-11 22:24:39 +0800

    Created new file     -JS-Beginner/operators.js     -JS-Beginner/variables.js
    Updated file:
        -JS-Beginner/index.html
    	-JS-Beginner/index.js

---
+++ JS-Beginner/operators.js
@@ -0,0 +1,41 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - *  /)
+//                        ex 11 = x + 5;
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 3
+// students = students % 3
+
+// Augmented Assignment
+
+// students += 3;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// increment / decrement
+// students++;
+// students--;
+
+console.log(students);
+
+// operator precedence
+
+/* 
+    1.parenthesis ()
+    2.exponents
+    3.multiplication & division & modulo
+    4.addition & subtraction
+
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result)
